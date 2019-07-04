ab4efaf6f0869230d10b8e384657190f88bbd2d5
MihiriKD <binojishara21@gmail.com>
2019-07-05 00:11:54 +0530

    setup bootestrap

--- mern-todo-app/src/App.js
+++ mern-todo-app/src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      
+        <div className="container">
+          <h2>MERN-Stack Todo App</h2>
+          <button type="button" class="btn btn-outline-primary">Primary</button>
+        </div>
+      
+    );
+  }
 }
 
 export default App;
