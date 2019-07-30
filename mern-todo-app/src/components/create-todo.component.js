ece806a2bfba2e49bf38630bbdbb4bcd7f0d7287
MihiriKD <binojishara21@gmail.com>
2019-07-30 23:05:45 +0530

    added form groups for several fields in create-todo-component

--- mern-todo-app/src/components/create-todo.component.js
+++ mern-todo-app/src/components/create-todo.component.js
@@ -62,95 +62,81 @@ export default class CreateTodo extends Component {
             shops:[]
         })
     }
-      render() {
+    render() {
         return (
-            <div style={{marginTop: 10}}>
+            <div style={{ marginTop: 10 }}>
                 <h3>Create New Phone</h3>
                 <form onSubmit={this.onSubmit}>
-                <div class="container">
-                <div class="row">
-                <div class="col-sm">
-                    <div className="form-group"> 
-                        <label>Name : </label>
-                        <input  type="text"
-                                name="phone_name"
-                                className="form-control"
-                                value={this.state.phone_name}
-                                onChange={this.onChangenameDescription}
-                                />
-                    </div>
-                    <div className="form-group">
-                    <label>Model : </label>
-                        <select className="custom-select">                            
-                            <option value="Samsung">Samsung</option>
-                            <option value="Huawei">Huawei</option>
-                            <option value="Apple">Apple</option>
-                            <option value="Xiaomi">Xiaomi</option>
-                            <option value="Nokia">Nokia</option>
-                            <option value="Oppo">Oppo</option>
-                            <option value="Vivo">Vivo</option>
-                            <option value="Asus">Asus</option>
-                            <option value="Lenovo">Lenovo</option>
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <label for="exampleFormControlSelect1">Ram</label>
-                        <select className="form-control" id="exampleFormControlSelect1">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>6</option>
-                            <option>8</option>
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <label for="exampleFormControlSelect1">Rom</label>
-                        <select className="form-control" id="exampleFormControlSelect1">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>4</option>
-                            <option>8</option>
-                            <option>16</option>
-                            <option>32</option>
-                            <option>64</option>
-                            <option>128</option>
-                        </select>
-                    </div>
-                    <div className="form-group">
-                        <div className="form-check form-check-inline">
-                            <input  className="form-check-input" 
-                                    type="radio" 
-                                    name="priorityOptions" 
-                                    id="priorityLow" 
-                                    value="Low"
-                                    checked={this.state.todo_priority==='Low'} 
-                                    onChange={this.onChangeTodoPriority}
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-sm">
+                                <div className="form-group">
+                                    <label>Name : </label>
+                                    <input type="text"
+                                        name="phone_name"
+                                        className="form-control"
+                                        value={this.state.phone_name}
+                                        onChange={this.onChangenameDescription}
                                     />
-                            <label className="form-check-label">Low</label>
+                                </div>
+                                <div className="form-group">
+                                    <label>Model : </label>
+                                    <select className="custom-select">
+                                        <option value="Samsung">Samsung</option>
+                                        <option value="Huawei">Huawei</option>
+                                        <option value="Apple">Apple</option>
+                                        <option value="Xiaomi">Xiaomi</option>
+                                        <option value="Nokia">Nokia</option>
+                                        <option value="Oppo">Oppo</option>
+                                        <option value="Vivo">Vivo</option>
+                                        <option value="Asus">Asus</option>
+                                        <option value="Lenovo">Lenovo</option>
+                                    </select>
+                                </div>
+                                <div className="form-group">
+                                    <label for="exampleFormControlSelect1">Ram</label>
+                                    <select className="form-control" id="exampleFormControlSelect1">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>6</option>
+                                        <option>8</option>
+                                    </select>
+                                </div>
+                                <div className="form-group">
+                                    <label for="exampleFormControlSelect1">Rom</label>
+                                    <select className="form-control" id="exampleFormControlSelect1">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>4</option>
+                                        <option>8</option>
+                                        <option>16</option>
+                                        <option>32</option>
+                                        <option>64</option>
+                                        <option>128</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className="col-sm">
+                                <div className="form-row">
+                                   <div className="form-group">
+                                        <label>Selfie Camera</label>
+                                        <input className="form-control" type="number" placeholder="Enter MP value"></input>
+                                    </div>
+                                  
+                                    <div className="form-group">
+                                        <label>Rear Camera</label>                               
+                                        <input className="form-control" type="number" placeholder="Enter MP value"></input>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className="form-check form-check-inline">
-                            <input  className="form-check-input" 
-                                    type="radio" 
-                                    name="priorityOptions" 
-                                    id="priorityMedium" 
-                                    value="Medium" 
-                                    checked={this.state.todo_priority==='Medium'} 
-                                    onChange={this.onChangeTodoPriority}
-                                    />
-                            <label className="form-check-label">Medium</label>
+                        <div className="row">
+                            <div className="form-group">
+                                <input type="submit" value="Create Todo" className="btn btn-primary" />
+                            </div>
                         </div>
-                    
-                    </div>
-
-                    <div className="form-group">
-                        <input type="submit" value="Create Todo" className="btn btn-primary" />
-                    </div>
-                    </div>
-                    <div class="col-sm">
-
-                    </div>
-                    </div>                    
                     </div>
                 </form>
             </div>
