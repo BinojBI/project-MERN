2063fe45e3ac5a9173de5b4f5f4da0b453aebe78
BinojBI <binojishara21@gmail.com>
2019-10-04 19:48:53 +0530

    completed phone info

--- mern-todo-app/src/components/show-phone-info.jsx
+++ mern-todo-app/src/components/show-phone-info.jsx
@@ -8,25 +8,82 @@ const phonePic={
     width:"auto"
 }
 
-
 export default class ShowPhoneInfo extends Component {
+
+    constructor(props){
+        super(props);
+        
+        this.titleCase = this.titleCase.bind(this);
+        this.getstores = this.getstores.bind(this);
+
+        this.state={
+            shops:null,
+            phoneName:"",
+            numofShops:0,
+            prices:[],
+            model:"",
+            ram:0,
+            rom:0,
+            rearCamera:0,
+            selfieCamera:0,
+            YearOfProduce:0,
+            battery:0,
+            network:0,
+            duelSim:false,
+
+        }
+    }
 
     componentDidMount() {
         axios.get('http://localhost:4000/phones/'+this.props.match.params.id)
             .then(response => {
+                let result = this.getstores(response.data.availableShops);
                 this.setState({
-                    todo_description: response.data.todo_description,
-                    todo_responsible: response.data.todo_responsible,
-                    todo_priority: response.data.todo_priority,
-                    todo_completed: response.data.todo_completed
-                })   
+                    shops: response.data.availableShops,
+                    phoneName: response.data.phone_name,
+                    numofShops:response.data.availableShops.length,
+                    prices:result,
+                    model: response.data.model,
+                    ram: response.data.ram,
+                    rom: response.data.rom,
+                    rearCamera: response.data.rear_camera,
+                    selfieCamera: response.data.selfie_camera,
+                    YearOfProduce: response.data.year,
+
+                }, ()=>{console.log(response.data)})   
             })
             .catch(function (error) {
                 console.log(error);
             })
     }
 
+    titleCase (str) {
+        var splitStr = str.toLowerCase().split(' ');
+        for (var i = 0; i < splitStr.length; i++) {
+            splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+        }
+        return splitStr.join(' '); 
+     }
+
+    getstores(shops){
+       let result = shops.map(a => a.phone_price)
+        return result;
+    }
+
     render() {
+
+        const rows = this.state.shops && this.state.shops.map((record,index) => {   
+            
+            return (
+                <tr key={index}>
+                <th width="150px">{record.shop_name}</th>
+                    <td>{record.phone_color}</td>
+                    <td>{record.phone_price}</td>
+                </tr>  
+
+            );
+        });
+
         return (
             <div>
                 <div className="container"  style={{marginTop:20}}>
@@ -35,34 +92,88 @@ export default class ShowPhoneInfo extends Component {
                             <img src={Pic} className="img-fluid" alt="Responsive image" style={phonePic}></img>
                         </div>
                         <div className="col-md-8">
-                            <div className="Heading">
-                                <h3 style={{marginBottom:0}}>Nokia 6.1Plus</h3>
+                            <div className="row">
+                            <div className="col-md-12 Heading">
+                            <h2 style={{marginBottom:0}}>{this.titleCase(this.state.phoneName)}</h2>
                             </div>
-                            <div>Buyer Ratings
-                                <div>
-                                <FontAwesome name='rocket' />
-                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="col-md-5">
+                                <div>Buyer Ratings
+                               
+                               <i className="fa fas-star"></i>&nbsp;&nbsp;
+                               <i className="fa fa-star"></i>&nbsp;
+                               <i className="fa fa-star"></i>&nbsp;
+                               <i className="fa fa-star"></i>&nbsp;
+                               <i className="fa fa-star"></i>
                                </div>
+                               <div>
+                                   <h5 style={{display:"inline", color:"#4CC417"}}>Available at</h5>&nbsp;&nbsp;
+                                   <h3  style={{display:"inline", color:"#38ACEC"}}>{this.state.numofShops} {this.state.numofShops>1?"stores":"store"}</h3>&nbsp;                                                                
+                               </div> 
+                                </div>
+                                
+                                <div className="col-md-7"  style={{padding:12}} >
+                                    <h6 style={{display:"inline"}} >{this.state.prices.length>1?"from ":"at "} Rs. </h6>
+                                    <h1 style={{display:"inline", color:"#4CC417", fontFamily:"fantasy"}} >{this.state.prices[0]}</h1>
+                                    {this.state.prices.length>1?
+                                    <div style={{display:"inline"}}>
+                                   <h6 style={{display:"inline"}}> to</h6>
+                                    <h1 style={{color:"#FFC321", fontFamily:"fantasy"}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{this.state.prices[this.state.prices.length-1]}</h1></div>
+                                    :""
+                                }
+                                </div>
+                                </div>
+                                <div className="row">
+                                    <div className="col-md-12">
+                                    <table className="table">
+                                        <tbody>
+                                              {rows}                                      
+                                        </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                           
+                                              
                         </div>
                     </div>
                     <div className="row"  style={{marginTop:40}}>
                         <div className="col-md-12">
+                            <div className="heading">
+                                <h4>Specification</h4>
+                            </div>
                         <table className="table table-striped">
                         <tbody>
                             <tr>
-                            <th width="200px">Row</th>
-                                <td>1</td>
+                            <th width="200px">Model</th>
+                                <td>{this.state.model}</td>
                             </tr>
                             <tr>
-                            <th>First Name</th>
-                                <td>John</td>                               
+                            <th>Ram</th>
+                                <td>{this.state.ram}</td>                               
                             </tr>
                             <tr>
-                            <th>Last Name</th>
-                                <td>Carter</td>                                
+                            <th>Rom</th>
+                                <td>{this.state.rom}</td>                                
                             </tr>
                             <tr>
-                            <th>Email</th>
+                            <th>Rear Camera</th>
+                                <td>{this.state.rearCamera}</td>                                
+                            </tr>
+                            <tr>
+                            <th>Selfie Camera</th>
+                                <td>{this.state.selfieCamera}</td>                                
+                            </tr>
+                            <tr>
+                            <th>Year of produce</th>
+                                <td>{this.state.YearOfProduce}</td>                                
+                            </tr>
+                            <tr>
+                            <th>Battery</th>
+                                <td>johncarter@mail.com</td>                                
+                            </tr>
+                            <tr>
+                            <th>Sim slots</th>
                                 <td>johncarter@mail.com</td>                                
                             </tr>
                         </tbody>
